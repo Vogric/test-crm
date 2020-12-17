0304b4306892109ee3fcff19fb24f9e052880cee
vogric <blvogric@gmail.com>
2020-12-17 16:50:24 -0300

    Adding Formik to SignIn

--- pages/signin.js
+++ pages/signin.js
@@ -1,6 +1,20 @@
 import Layout from "../components/Layout";
+import { useFormik } from "formik";
+import * as Yup from "yup";
 
 const SignIn = () => {
+  const formik = useFormik({
+    initialValues: {
+      name: "",
+      surname: "",
+      email: "",
+      password: "",
+    },
+    onSubmit: (values) => {
+      console.log(values);
+    },
+  });
+
   return (
     <Layout>
       <h1 className="text-center text-4xl text-gray-200 pt-20 pb-4 ">
@@ -8,7 +22,10 @@ const SignIn = () => {
       </h1>
       <div className="flex justify-center mt-5">
         <div className="w-full max-w-sm">
-          <form className="bg-white rounded shadow-md px-8 pt-6 pb-8 mb-4">
+          <form
+            onSubmit={formik.handleSubmit}
+            className="bg-white rounded shadow-md px-8 pt-6 pb-8 mb-4"
+          >
             <div className="mb-4">
               <label
                 className="block text-blue-700 text-sm font-bold mb-2"
@@ -21,6 +38,8 @@ const SignIn = () => {
                 id="name"
                 placeholder="User name"
                 type="text"
+                value={formik.values.name}
+                onChange={formik.handleChange}
               />
             </div>
             <div className="mb-4">
@@ -35,6 +54,8 @@ const SignIn = () => {
                 id="surname"
                 placeholder="User surname"
                 type="text"
+                value={formik.values.surname}
+                onChange={formik.handleChange}
               />
             </div>
             <div className="mb-4">
@@ -49,6 +70,8 @@ const SignIn = () => {
                 id="email"
                 placeholder="User email"
                 type="email"
+                value={formik.values.email}
+                onChange={formik.handleChange}
               />
             </div>
             <div className="mb-4">
@@ -63,6 +86,8 @@ const SignIn = () => {
                 id="password"
                 placeholder="User password"
                 type="password"
+                value={formik.values.password}
+                onChange={formik.handleChange}
               />
             </div>
             <input
