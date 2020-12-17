5ff82ca470f9c046081a039c5fbb47ed24ea6f7f
vogric <blvogric@gmail.com>
2020-12-17 16:58:59 -0300

    Adding Formik to Login

--- pages/login.js
+++ pages/login.js
@@ -1,12 +1,27 @@
 import Layout from "../components/Layout";
+import { useFormik } from "formik";
+import * as Yup from "yup";
 
 const Login = () => {
+  const formik = useFormik({
+    initialValues: {
+      email: "",
+      password: "",
+    },
+    onSubmit: (values) => {
+      console.log(values);
+    },
+  });
+
   return (
     <Layout>
       <h1 className="text-center text-4xl text-gray-200 mt-40 pb-4 ">Login</h1>
       <div className="flex justify-center mt-5">
         <div className="w-full max-w-sm">
-          <form className="bg-white rounded shadow-md px-8 pt-6 pb-8 mb-4">
+          <form
+            onSubmit={formik.handleSubmit}
+            className="bg-white rounded shadow-md px-8 pt-6 pb-8 mb-4"
+          >
             <div className="mb-4">
               <label
                 className="block text-blue-700 text-sm font-bold mb-2"
@@ -19,6 +34,8 @@ const Login = () => {
                 id="email"
                 placeholder="User email"
                 type="email"
+                values={formik.values.email}
+                onChange={formik.handleChange}
               />
             </div>
             <div className="mb-4">
@@ -33,6 +50,8 @@ const Login = () => {
                 id="password"
                 placeholder="User password"
                 type="password"
+                values={formik.values.password}
+                onChange={formik.handleChange}
               />
             </div>
             <input
